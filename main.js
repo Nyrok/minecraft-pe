6db70b2e6db47ec2c88d2efd1d517c97427b316d
Nyrok <47859726+Nyrok@users.noreply.github.com>
2021-07-13 19:49:59 +0200

    1.0.0

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ require('colors');
 
 module.exports = class Connect {
     constructor(server_api_key) {
-        this.server_api_key = server_api_key
+        this.server_api_key = server_api_key;
 
         /**
          * Get JSON informations of your Minecraft BE Server.
@@ -13,16 +13,19 @@ module.exports = class Connect {
         this.getInformations = async () => {
             return await new Promise(async (resolve, reject) => {
                 if (typeof this.server_api_key !== "string" || this.server_api_key.length !== 35 || !this.server_api_key)
-                    return reject(new Error("[minecraft-pe] The provided Server API Key isn't a valid API Key.".red))
+                    return reject(new Error("[minecraft-pe] The provided Server API Key isn't a valid API Key.".red));
                 return await fetch(`https://minecraftpocket-servers.com/api/?object=servers&element=detail&key=${server_api_key}`)
                     .then(async function (request) {
-                        return resolve(await request.json())
+                        return resolve(request.json().then(data => {
+                            return data;
+                        }).catch(function (error) {
+                            throw reject(new Error(`[minecraft-pe] Bad Request Response. ${error}`.red));
+                        }));
                     })
-                    .catch(async function (error) {
-                        throw reject(new Error(`[minecraft-pe] I couldn't make this request. ${error}`.red))
+                    .catch(function (error) {
+                        throw reject(new Error(`[minecraft-pe] I couldn't make this request. ${error}`.red));
                     })
             })
         }
     }
 }
-
